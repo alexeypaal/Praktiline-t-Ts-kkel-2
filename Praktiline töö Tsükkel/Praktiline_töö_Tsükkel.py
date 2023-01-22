5c8e3c76647edca1e73e37873b4fbff269fc1dbd
wasabi <wasabi@DESKTOP-R4M059E>
2023-01-22 23:40:35 +0200

    Добавьте файлы проекта.

---
+++ Praktiline töö Tsükkel/Praktiline_töö_Tsükkel.py
@@ -0,0 +1,34 @@
+﻿#1
+from math import *
+from random import *
+sum = 0
+i = 0
+while i < 10:
+    num = input("Sisestage number: ")
+    if num == "":
+        break
+    sum += int(num)
+    i += 1
+print("Arvude summa on:", sum)
+print()
+
+#2
+sum = 0
+for i in range(10):
+    num = input("Sisestage number: ")
+    if num == "":
+        break
+    sum += int(num)
+print("Arvude summa on:", sum)
+print()
+
+
+#3
+sum = 0
+while True:
+    num = input("Sisestage number või vajutage väljumiseks sisestusklahvi: ")
+    if num == "":
+        break
+    sum += int(num)
+print("Arvude summa on:", sum)
+print()
